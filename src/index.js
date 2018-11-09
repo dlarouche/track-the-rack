b2bf041e53d8b83e64f580a44a128c752b6aef54
David Larouche <dlarouche17@gmail.com>
2018-11-08 23:52:35 -0500

    Add custome theme

--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
+import { createMuiTheme } from '@material-ui/core/styles';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#718da2'
+    },
+    secondary: {
+      main: '#ffffff'
+    }
+  },
+});
 
 ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <MuiThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </MuiThemeProvider>
 ), document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
