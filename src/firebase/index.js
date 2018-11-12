4d3f8817cac1acb83842d3828a37ae1d2544ae18
David Larouche <dlarouche17@gmail.com>
2018-11-11 22:23:43 -0500

    Set up firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,22 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyD5un_OIp8bnwaOpXcQhG4j5Taeus-vhyY",
+  authDomain: "track-the-rack.firebaseapp.com",
+  databaseURL: "https://track-the-rack.firebaseio.com",
+  projectId: "track-the-rack",
+  storageBucket: "track-the-rack.appspot.com",
+  messagingSenderId: "562811217603"
+};
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+function getMeals() {
+  return database.ref('/meals/').once('value').then(snapshot => snapshot.val())
+}
+
+export default {
+  getMeals
+}
